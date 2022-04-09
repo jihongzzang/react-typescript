baf9a6a7d3218fd2273b6d9227c4c86df400be4c
jihongzzang <jujihong2@gmail.com>
2022-04-09 14:17:34 +0900

    Add: CarouselUi

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 - [Skeleton UI](src/Components/Skeleton/Skeleton.md)
 - [Skeleton UI 컴포넌트](src/Components/Skeleton/Skeleton.tsx)
 - [Skeleton UI 예제코드](src/Components/Skeleton/ExampleSkeleton.tsx)
+
+#### **Carousel UI**
+
+- [Carousel UI](src/Components/Carousel/Carousel.md)
+- [Carousel UI 컴포넌트](src/Components/Carousel/Carousel.tsx)
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import ExampleSkeleton from 'Components/Skeleton/ExampleSkeleton';
+import Carousel from 'Components/Carousel/Carousel';
+// import ExampleSkeleton from 'Components/Skeleton/ExampleSkeleton';
 
 function App() {
   return (
     <>
-      <ExampleSkeleton />
+      {/* <ExampleSkeleton /> */}
+      <Carousel />
     </>
   );
 }
